74434e56220698b1d356845f5a5b056e8cbd3a95
SwarShah <swar12894@gmail.com>
2015-06-30 17:26:38 -0400

    ASCII faces demo

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 var express = require('express');
 var app = express();
+var cool = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
 app.get('/', function(request, response) {
-  response.render('pages/index')
+  response.send(cool());
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
+  console.log("Node app is running on port:" + app.get('port'))
+})
