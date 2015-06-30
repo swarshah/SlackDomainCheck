09ffe3c341368b24addacb488830e6ffbc127bc0
SwarShah <swar12894@gmail.com>
2015-06-30 18:10:43 -0400

    Removed bodyParser for JSON

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var dns = require('dns');
 var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
   response.send('Hello there!');
@@ -10,15 +8,16 @@ app.get('/', function(request, response) {
 
 app.post('/', function(req, res) {
 	console.log(req.body);
+	console.log(req.body.text);
 	var domain = req.body.text;
-	dns.resolve4(domain, function (err, addresses) {
+	/*dns.resolve4(domain, function (err, addresses) {
 		if (err){
 			res.send('This domain is availabel!');
 		}
 		else{
 			res.send('This domain is not available!');
 		}
-	});
+	});*/
 });
 
 app.set('port', (process.env.PORT || 5000));
