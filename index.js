6b66c00e2353a0ff3305b9b89926a5d19e575cdb
SwarShah <swar12894@gmail.com>
2015-07-12 14:31:00 -0400

    Removed unwanted files and added comments

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var dns = require('dns');
 var app = express();
-
 var bodyParser = require('body-parser')
+
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
+
+//Just a message for GET!
 app.get('/', function(request, response) {
   response.send('Hello there!');
 });
 
 app.post('/', function(req, res) {
-	//console.log(req.body);
 	console.log(req.body.text);
 	var domain = req.body.text;
+	//DNS resolve to check if domain is available or not
 	dns.resolve4(domain, function (err, addresses) {
 		if (err){
 			res.send('Looks like *'+domain+'* is available!');
